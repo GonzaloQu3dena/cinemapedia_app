f659f350ffcbd8d5bd2e65ded3ae83266977725b
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2024-12-26 15:37:51 -0500

    refactor(cinema): rename data source method.

--- lib/features/cinema/domain/datasources/movies_datasource.dart
+++ lib/features/cinema/domain/datasources/movies_datasource.dart
@@ -2,24 +2,23 @@ import 'package:cinemapedia_app/features/cinema/domain/entities/movie.dart';
 
 /// ### Movies Data Source
 /// This class is an abstract class that will be implemented by the data source layer.
-/// 
-/// #### Methods: 
+///
+/// #### Methods:
 /// - [getCinemasByPage] - This method will be implemented by the data source layer to get the list of movies by page.
-/// 
+///
 /// #### Author:
 /// Gonzalo Quedena
 abstract class MoviesDatasource {
-
   /// ### Get the list of movies by page.
   /// This method will be implemented by the data source layer to get the list of movies by page.
-  /// 
+  ///
   /// #### Parameters:
   /// - [pageNumber]: The page number to get the list of movies.
-  /// 
+  ///
   /// #### Returns:
   /// - A list of movies.
-  /// 
+  ///
   /// #### Author:
   /// Gonzalo Quedena
-  Future<List<Movie>> getMoviesByPage({int pageNumber = 1});
+  Future<List<Movie>> getNowPlaying({int pageNumber = 1});
 }
