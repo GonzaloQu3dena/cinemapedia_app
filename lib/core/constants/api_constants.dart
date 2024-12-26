13971c048b16745c191c22437ba8b90c0b7f0931
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2024-12-26 15:36:18 -0500

    refactor(core): add constant value to API base URL.

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,15 +1,17 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Api Constants
-/// 
+///
 /// This class is used to store the api constants. [baseUrl] is the base url of the api and [apiKey] is the api key to access the api.
-/// 
+///
 /// Properties:
 /// * [apiKey]: Is a constant String that is used to access the api.
 /// * [baseUrl]: Is a constant String that is the base url of the api.
-/// 
+///
 /// Author: Gonzalo Quedena
 class ApiConstants {
-  static const String baseUrl = "";
-  static String apiKey = dotenv.env["THE_MOVIEDB_API_KEY"] ?? "API Key not found";
+  static String apiKey =
+      dotenv.env["THE_MOVIEDB_API_KEY"] ?? "API Key not found";
+      
+  static const String baseUrl = "https://api.themoviedb.org/3";
 }
