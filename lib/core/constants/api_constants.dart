44daa2fb55d5dda8508c8bdf1138c29119c7a9e8
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2024-12-26 00:08:43 -0500

    feat(core): add reusable elements.

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,13 @@
+/// Api Constants
+/// 
+/// This class is used to store the api constants. [baseUrl] is the base url of the api and [apiKey] is the api key to access the api.
+/// 
+/// Properties:
+/// * [apiKey]: Is a constant String that is used to access the api.
+/// * [baseUrl]: Is a constant String that is the base url of the api.
+/// 
+/// Author: Gonzalo Quedena
+class ApiConstants {
+  static const String apiKey = "";
+  static const String baseUrl = "";
+}
---
+++ lib/core/errors/failure.dart
@@ -0,0 +1,39 @@
+/// ### Failure 
+/// 
+/// This class is used to handle errors in the application.
+/// 
+/// #### Properties:
+/// * [message] is the error message.
+/// 
+/// #### Author: 
+/// Gonzalo Quedena
+abstract class Failure {
+  final String message;
+  const Failure(this.message);
+}
+
+/// ### ServerFailure
+/// 
+/// This class is used to handle server errors in the application.
+/// 
+/// #### Properties:
+/// * [message] is the error message.
+/// 
+/// #### Author: 
+/// Gonzalo Quedena
+class ServerFailure extends Failure {
+  const ServerFailure(super.message);
+}
+
+/// ### CacheFailure
+/// 
+/// This class is used to handle cache errors in the application.
+/// 
+/// #### Properties:
+/// * [message] is the error message.
+/// 
+/// #### Author: 
+/// Gonzalo Quedena
+class CacheFailure extends Failure {
+  const CacheFailure(super.message);
+}
