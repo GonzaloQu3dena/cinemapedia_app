5364f247a82b2fa6652129ae0265872cab095747
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2024-12-27 21:45:27 -0500

    feat(cinema): add helper to format numbers in a human-readable format.

---
+++ lib/core/helpers/human_formats.dart
@@ -0,0 +1,26 @@
+import 'package:intl/intl.dart';
+
+/// ### Human Formats
+/// This class contains methods to format numbers in a human-readable way.
+/// 
+/// #### Methods:
+/// - [number] - Formats a number to a human-readable format. It uses the [NumberFormat.compactCurrency] method from the `intl` package.
+/// 
+/// #### Author:
+/// Gonzalo Quedena
+class HumanFormats {
+
+  /// Formats a number to a human-readable format. It uses the [NumberFormat.compactCurrency] method from the `intl` package.
+  static String number( double number ) {
+
+    /// Formats the number to a human-readable format.
+    final formattedNumber = NumberFormat.compactCurrency(
+      decimalDigits: 0,
+      symbol: '',
+      locale: 'en'
+    ).format(number);
+
+    return formattedNumber;
+  }
+
+}
