88e1e59b62dc8fc8187938ed1f5a18d264fe66a6
GonzaloQu3dena <gonzalo.qu3dena@gmail.com>
2025-01-04 12:17:58 -0500

    refactor(core): add number of decimals to number formatter.

--- lib/core/helpers/human_formats.dart
+++ lib/core/helpers/human_formats.dart
@@ -11,11 +11,11 @@ import 'package:intl/intl.dart';
 class HumanFormats {
 
   /// Formats a number to a human-readable format. It uses the [NumberFormat.compactCurrency] method from the `intl` package.
-  static String number( double number ) {
+  static String number( double number, [int decimals = 0] ) {
 
     /// Formats the number to a human-readable format.
     final formattedNumber = NumberFormat.compactCurrency(
-      decimalDigits: 0,
+      decimalDigits: decimals,
       symbol: '',
       locale: 'en'
     ).format(number);
